06a3034b147759b0f737b9b45ff69202cfaeaf55
Adam Cragg <adam.cragg1245@gmail.com>
2024-06-20 17:39:27 -0700

    Updating CodingChallenge with my final implementation

--- src/components/CodingChallenge.vue
+++ src/components/CodingChallenge.vue
@@ -1,20 +1,69 @@
 <template>
-  <div>
+  <div width="400" height="400">
     <h1>Options Profit Calculator</h1>
-    {{ "Your Code Here" }}
+    <div>
+      <p>Max Profit: {{ maxProfit }}</p>
+      <p>Max Loss: {{ maxLoss }}</p>
+      <p>Break-even Points: {{ breakeven }}</p>
+    </div>
+    <canvas id="optionsGraph"></canvas>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'CodingChallenge',
-  props: {
-    optionsData: Array
+  import { calculateBreakevenPoints, calculateGainsForEachOption, calculateGraphData, calculateMaxLoss, calculateMaxProfit, drawGraph, isNullOrUndefined } from '../utilities';
+  export default {
+    name: 'CodingChallenge',
+    computed: {
+      breakeven() {
+        const breakeven = this.breakEvenPoints
+          .filter((data) => !isNullOrUndefined(data) && typeof data === 'number')
+          .map((data) => data.toFixed(2))
+          .join(', ')
+        if (breakeven === '') {
+          return 'There are none';
+        }
+        return breakeven;
+      }
+    },
+    data() {
+      return {
+        breakEvenPoints: [],
+        graphData: [],
+        maxLoss: 0,
+        maxProfit: 0
+      };
+    },
+    methods: {
+      calculateBreakevenPoints() {
+        return calculateBreakevenPoints(this.graphData);
+      },
+      calculateGraphData() {
+        const lookup = calculateGainsForEachOption(this.optionsData);
+        return calculateGraphData(lookup);
+      },
+      calculateMaxLoss() {
+        return calculateMaxLoss(this.graphData);
+      },
+      calculateMaxProfit() {
+        return calculateMaxProfit(this.graphData);
+      },
+      drawGraph() {
+        drawGraph(this.graphData, document.getElementById('optionsGraph'));
+      }
+    },
+    mounted() {
+      this.graphData = this.calculateGraphData();
+      this.maxProfit = this.calculateMaxProfit();
+      this.maxLoss = this.calculateMaxLoss();
+      this.breakEvenPoints = this.calculateBreakevenPoints()
+      this.drawGraph();
+    },
+    props: {
+      optionsData: Array
+    }
   }
-  // Your code here
-}
 </script>
 
 <style scoped>
-/* Your Code Here */
 </style>
